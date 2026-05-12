441ee15261f9aa61d9fe0df5f512238ee3163f9c
Yurii <yurikuzn@users.noreply.github.com>
2026-05-12 13:52:51 +0300

    Export toVNode

--- src/entry.ts
+++ src/entry.ts
@@ -2,7 +2,7 @@
 import Events from './bull.events.js';
 import View from './bull.view.js';
 import Factory from './bull.factory.js';
-import {h, fragment} from 'snabbdom';
+import {h, fragment, toVNode} from 'snabbdom';
 
 export {VNode, VNodeData} from './module-util';
-export {View, Events, Factory, h, fragment};
+export {View, Events, Factory, h, fragment, toVNode};
